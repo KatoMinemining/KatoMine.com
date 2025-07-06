2d9b506c6f2a95bf5d069940dbc86a019d5718e3
KatoMinemining <ainetworkmining22@gmail.com>
2025-07-06 22:00:14 +0600

    Update firebase config.js

--- firebase config.js
+++ firebase config.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// এই স্ক্রিপ্ট ব্যবহার করার আগে HTML এ অবশ্যই firebase-app.js এবং firebase-database.js লিঙ্ক যুক্ত থাকবে।
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB9R2gdGuV987lpJ3tLWEqVpFu7c2gnNOs",
-  authDomain: "katomine-95b44.firebaseapp.com",
-  projectId: "katomine-95b44",
-  storageBucket: "katomine-95b44.firebasestorage.app",
-  messagingSenderId: "1072386418425",
-  appId: "1:1072386418425:web:7f2ce6ff3ccce7b141e428",
-  measurementId: "G-1G2682BWVV"
+    apiKey: "AIzaSyB9R2gdGuV987lpJ3tLWEqVpFu7c2gnNOs", // নতুন key নিলে এখানে আপডেট করবে
+    authDomain: "katomine-95b44.firebaseapp.com",
+    databaseURL: "https://katomine-95b44-default-rtdb.firebaseio.com",
+    projectId: "katomine-95b44",
+    storageBucket: "katomine-95b44.appspot.com",
+    messagingSenderId: "1072386418425",
+    appId: "1:1072386418425:web:7f2ce6ff3ccce7b141e428",
+    measurementId: "G-1G2682BWVV"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebase Initialize
+firebase.initializeApp(firebaseConfig);
+
+// Realtime Database reference
+const database = firebase.database();
+
+// ডাটাবেজে রিড/রাইট করার জন্য রেফারেন্স ব্যবহার উদাহরণ:
+// database.ref('users/userId').set({name: "Test User"});
+// database.ref('users/userId').on('value', (snapshot) => { console.log(snapshot.val()); });
