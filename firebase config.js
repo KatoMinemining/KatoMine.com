3e117c99a5ed1212854e409ba27edf9fe7cb2d7c
KatoMinemining <ainetworkmining22@gmail.com>
2025-07-10 19:08:25 +0600

    Update config.js

--- firebase config.js
+++ firebase config.js
@@ -1,24 +1,11 @@
-// firebase-config.js
-
-// এই স্ক্রিপ্ট ব্যবহার করার আগে HTML এ অবশ্যই firebase-app.js এবং firebase-database.js লিঙ্ক যুক্ত থাকবে।
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB9R2gdGuV987lpJ3tLWEqVpFu7c2gnNOs", // নতুন key নিলে এখানে আপডেট করবে
-    authDomain: "katomine-95b44.firebaseapp.com",
-    databaseURL: "https://katomine-95b44-default-rtdb.firebaseio.com",
-    projectId: "katomine-95b44",
-    storageBucket: "katomine-95b44.appspot.com",
-    messagingSenderId: "1072386418425",
-    appId: "1:1072386418425:web:7f2ce6ff3ccce7b141e428",
-    measurementId: "G-1G2682BWVV"
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCeCNWpgo32Yr5wBKo_A2VW33Bc8PQXmV8",
+  authDomain: "katomineconnect-90087.firebaseapp.com",
+  databaseURL: "https://katomineconnect-90087-default-rtdb.firebaseio.com",
+  projectId: "katomineconnect-90087",
+  storageBucket: "katomineconnect-90087.firebasestorage.app",
+  messagingSenderId: "1070653902206",
+  appId: "1:1070653902206:web:a32d98f1bac28f8ebff72f",
+  measurementId: "G-H9GF2BQX34"
 };
-
-// Firebase Initialize
-firebase.initializeApp(firebaseConfig);
-
-// Realtime Database reference
-const database = firebase.database();
-
-// ডাটাবেজে রিড/রাইট করার জন্য রেফারেন্স ব্যবহার উদাহরণ:
-// database.ref('users/userId').set({name: "Test User"});
-// database.ref('users/userId').on('value', (snapshot) => { console.log(snapshot.val()); });
